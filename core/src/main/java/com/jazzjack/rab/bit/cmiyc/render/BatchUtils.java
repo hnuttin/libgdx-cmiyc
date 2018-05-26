976be3776cbeca998d48fbb7f323a1e631f6b1dd
Hans Nuttin <hans.nuttin@aca-it.be>
2018-05-27 00:20:42 +0200

    fix sonar issue

--- core/src/main/java/com/jazzjack/rab/bit/cmiyc/render/BatchUtils.java
+++ core/src/main/java/com/jazzjack/rab/bit/cmiyc/render/BatchUtils.java
@@ -3,9 +3,11 @@ package com.jazzjack.rab.bit.cmiyc.render;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.Batch;
 
-public class BatchUtils {
+class BatchUtils {
 
-    public static void drawWithAlpha(Batch batch, float alpha, Runnable runnable) {
+    private BatchUtils() {}
+
+    static void drawWithAlpha(Batch batch, float alpha, Runnable runnable) {
         Color color = batch.getColor();
         batch.setColor(color.r, color.g, color.b, alpha);
         runnable.run();
